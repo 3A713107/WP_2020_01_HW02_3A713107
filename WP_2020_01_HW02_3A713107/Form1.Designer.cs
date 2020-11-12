6b8e30fbc3f88adf9aa09ca44c2f88ff4fc49a92
3A713107 <andd200088@gmail.com>
2020-11-12 21:28:33 +0800

    加入專案檔案。

---
+++ WP_2020_01_HW02_3A713107/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace WP_2020_01_HW02_3A713107
+{
+    partial class frmGame
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGO = new System.Windows.Forms.Button();
+            this.rtbMsg = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnDraw = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.btnDraw)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGO
+            // 
+            this.btnGO.Font = new System.Drawing.Font("新細明體", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnGO.Location = new System.Drawing.Point(473, 43);
+            this.btnGO.Name = "btnGO";
+            this.btnGO.Size = new System.Drawing.Size(135, 108);
+            this.btnGO.TabIndex = 0;
+            this.btnGO.Text = "抽";
+            this.btnGO.UseVisualStyleBackColor = true;
+            this.btnGO.Click += new System.EventHandler(this.btnGO_Click);
+            // 
+            // rtbMsg
+            // 
+            this.rtbMsg.Location = new System.Drawing.Point(109, 125);
+            this.rtbMsg.Name = "rtbMsg";
+            this.rtbMsg.Size = new System.Drawing.Size(210, 267);
+            this.rtbMsg.TabIndex = 2;
+            this.rtbMsg.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(159, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 48);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "歷程";
+            // 
+            // btnDraw
+            // 
+            this.btnDraw.Image = global::WP_2020_01_HW02_3A713107.Properties.Resources._001;
+            this.btnDraw.Location = new System.Drawing.Point(427, 175);
+            this.btnDraw.Name = "btnDraw";
+            this.btnDraw.Size = new System.Drawing.Size(234, 217);
+            this.btnDraw.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btnDraw.TabIndex = 4;
+            this.btnDraw.TabStop = false;
+            // 
+            // frmGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDraw);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rtbMsg);
+            this.Controls.Add(this.btnGO);
+            this.Name = "frmGame";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmGame_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.btnDraw)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGO;
+        private System.Windows.Forms.RichTextBox rtbMsg;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox btnDraw;
+    }
+}
+
---
+++ WP_2020_01_HW02_3A713107/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using WP_2020_01_HW02_3A713107.Properties;
+
+namespace WP_2020_01_HW02_3A713107
+{
+    public partial class frmGame : Form
+    {
+        List<Image> list;
+        Poker p = null;
+        List<int> Poker;
+
+
+        public frmGame()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDraw_Click(object sender, EventArgs e)
+        {
+            int index = 3;//模擬隨機產生 一個值;
+            btnDraw.Image = list[index];
+        }
+
+        int a = 0;
+        private void btnGO_Click(object sender, EventArgs e)
+        {
+            if (a < 30)
+            {
+                btnDraw.Image = list[Poker[a]];
+                a = a + 1;
+            }
+        }
+
+        private void frmGame_Load_1(object sender, EventArgs e)
+        {
+            p = new Poker();
+            Poker = p.GetPoker(30);
+            list = new List<Image>();
+
+            list.Add(Resources._001);
+            list.Add(Resources._002);
+            list.Add(Resources._007);
+            list.Add(Resources._004);
+            list.Add(Resources._006);
+            list.Add(Resources._009);
+            list.Add(Resources._012);
+            list.Add(Resources._013);
+            list.Add(Resources._014);
+            list.Add(Resources._016);
+            list.Add(Resources._017);
+            list.Add(Resources._018);
+            list.Add(Resources._019);
+            list.Add(Resources._020);
+            list.Add(Resources._021);
+            list.Add(Resources._025);
+            list.Add(Resources._026);
+            list.Add(Resources._028);
+            list.Add(Resources._029);
+            list.Add(Resources._031);
+            list.Add(Resources._032);
+            list.Add(Resources._033);
+            list.Add(Resources._034);
+            list.Add(Resources._035);
+            list.Add(Resources._036);
+            list.Add(Resources._037);
+            list.Add(Resources._038);
+            list.Add(Resources._040);
+            list.Add(Resources._042);
+            list.Add(Resources._044);
+            list.Add(Resources._048);
+            list.Add(Resources._050);
+        }
+    }
+}
